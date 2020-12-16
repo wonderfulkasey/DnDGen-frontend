85938df824a4b90e1473ddc075e75e4b4a6b5fae
wonderfulkasey <kaseyarnold15@gmail.com>
2020-12-16 18:01:41 -0500

    added postfetch function

--- src/index.js
+++ src/index.js
@@ -33,5 +33,27 @@ function createFormHandler(e) {
 }
 
 function postFetch(name, equipment_id) {
-  console.log(name, equipment_id)
+  console.log(name, equipment_id);
+  let bodyData = {name, equipment_id}
+
+  fetch(endPoint, {
+    // POST request
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify(bodyData)
+  })
+  .then(response => response.json())
+  .then(weapon => {
+    console.log(weapon);
+    const weaponData = weapon.data
+    // render JSON response
+    const weaponMarkup = `
+    <div data-id=${weapon.id}>
+      <h3>${weaponData.attributes.name}</h3>
+      <button data-id=${weaponData.id}>edit</button>
+    </div>
+    <br><br>`;
+
+    document.querySelector('#weapon-container').innerHTML += weaponMarkup;
+  })
 }
