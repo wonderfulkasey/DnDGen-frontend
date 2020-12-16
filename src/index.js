2bdd1fa27d1497e9e45e8f3af29577d431ad65e0
wonderfulkasey <kaseyarnold15@gmail.com>
2020-12-16 13:46:21 -0500

    added get weaponing function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
+const endPoint = "http://localhost:3000/weapons"
+
 document.addEventListener('DOMContentLoaded', () => {
-  alert('LOADED');
+  getWeaponing()
 });
+
+function getWeaponing() {
+  fetch(endPoint)
+    .then(response => response.json())
+    .then(weaponing => {
+      weaponing.data.forEach(weapon => {
+        const weaponMarkup = `
+          <div data-id=${weapon.id}>
+            <h3>${weapon.attributes.name}</h3>
+            <button data-id=${weapon.id}>edit</button>
+          </div>
+          <br><br>`;
+
+          document.querySelector('#weapon-container').innerHTML += weaponMarkup
+      })
+    })
+}
