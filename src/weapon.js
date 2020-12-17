117aba629f7007175afbf72013649306cc571a5d
wonderfulkasey <kaseyarnold15@gmail.com>
2020-12-17 13:00:14 -0500

    adding weapon class render

--- src/weapon.js
+++ src/weapon.js
@@ -1,1 +1,20 @@
-console.log("in weapon js");
+class Weapon {
+    constructor(id, weaponAttributes) {
+        this.id = id;
+        this.name = weaponAttributes.name;
+        this.equipment = weaponAttributes.equipment;
+        weapon.all.push(this);
+      }
+    
+      renderweaponCard() {
+        return `
+                <div data-id=${this.id}>
+                  <h3>${this.name}</h3>
+                  <p>${this.equipment.name}</p>
+                  <button data-id=${this.id}>edit</button>
+                </div>
+                <br><br>`;
+      }
+}
+
+Weapon.all = [];
