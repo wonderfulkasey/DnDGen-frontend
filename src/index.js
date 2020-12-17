e60d2d4a7b8bcf4e9d77fc820b9b2f1ea8c8674b
wonderfulkasey <kaseyarnold15@gmail.com>
2020-12-17 12:33:48 -0500

    making getweaponing function DRY

--- src/index.js
+++ src/index.js
@@ -14,14 +14,7 @@ function getWeaponing() {
     .then(response => response.json())
     .then(weaponing => {
       weaponing.data.forEach(weapon => {
-        const weaponMarkup = `
-          <div data-id=${weapon.id}>
-            <h3>${weapon.attributes.name}</h3>
-            <button data-id=${weapon.id}>edit</button>
-          </div>
-          <br><br>`;
-
-          document.querySelector('#weapon-container').innerHTML += weaponMarkup
+      render(weaponData)
       })
     })
 }
