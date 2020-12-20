1f59020c8d722f55b7340b2f8c5b4968163ffb4b
wonderfulkasey <kaseyarnold15@gmail.com>
2020-12-20 14:41:12 -0500

    changing up index js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-const BASE_URL = "http://localhost:3000/weapons"
+const BASE_URL = "http://localhost:3000"
+const WEAPONS_URL = `${BASE_URL}/weapons`
+
+
 
 class Weapon {
 
@@ -19,112 +22,21 @@ class Weapon {
 }}
 
 
-document.addEventListener('DOMContentLoaded', () => {
-  //fetches and gets weapon list
-  console.log("DOM is Loaded");
-  getWeaponing()
-
-  const createWeaponForm = document.querySelector('#create-weapon-form')
-  createWeaponForm.addEventListener('submit', (e) = createFormHandler(e))
-  // listens for 'submit' event on form and handles data
-  //const createWeaponForm = document.querySelector('#create-weapon-form')
-  //createWeaponForm.addEventListener('submit', (e) => createFormHandler(e))
-  const weaponContainer = document.querySelector('#weapon-container')
-  weaponContainer.addEventListener('click', e => {
-    
-    const id = parseInt(e.target.dataset.id);
-    const weapon = Weapon.findById(id);
-    document.querySelector('#update-weapon').innerHTML = weapon.renderUpdateForm();
-    //console.log('clicked');
-  });
-
-  document.querySelector('#update-weapon').addEventListener('submit', e => updateFormHandler(e))
-
-})
-
-function getWeaponing() {
-  fetch(endPoint)
-    .then(response => response.json())
-
-    .then(weaponing => {
-      weaponing.data.forEach(weapon => {
-        //makes a new instance of Weapon clss for each weapon in DB array
-        //let newWeapon = new Weapon(weapon, weapon.attributes)
-        const newWeapon = new Weapon(weapon.id, weapon.attributes)
-
-        //render newweaponcard located in weapon class
-        document.querySelector('#weapon-container').innerHTML += newWeapon.renderWeaponCard()
-      
-      })
-    })
+function fetchWeapons (){
+  fetch(WEAPONS_URL)
+  .then(res => res.json())
+  .then(weapons => renderWeapons(weapons))
 }
+ 
 
+function renderWeapons(weapons) {
+     weapons.forEach(weapon => {
+            innerHTML += `<div class="card">
+              <h3>${weapon.attributes.name}</h3>
+              <p>${weapon.attributes.equipment.name}</p>
+              <button data-id=${weapon.id}>edit</button>
+              </br>
+            </div>`
+    })       
+} 
 
-function createFormHandler(e) {
-  e.preventDefault()
-  const nameInput = document.querySelector('#input-name').value
-  const equipmentInput = document.querySelector('#equipments').value
-  const equipmentId = parseInt
-  postFetch(nameInput, equipmentInput)
-}
-
-function updateFormHandler(e) {
-  e.preventDefault();
-  const id = parseInt(e.target.dataset.id);
-  const weapon = Weapon.findById(id);
-  const name = e.target.querySelector('#input-name').value;
-  const equipment_id = parseInt(e.target.querySelector('#equipment').value);
-  patchWeapon(weapon, name, equipment_id)
-}
-
-function postFetch(name, equipment_id) {
-  //builds body object outside of fetch
-  //console.log(name, equipment_id);
-  const bodyData = {name, equipment_id}
-
-  fetch(endPoint, {
-    // POST request
-    method: "POST",
-    headers: {"Content-Type": "application/json"},
-    body: JSON.stringify(bodyData)
-  })
-  .then(response => response.json())
-  .then(weapon => {
-    //console.log(weapon);
-    //const weaponData = weapon.data
-    //render JSON response
-    const newWeapon = new Weapon(weapon.data.id, weapon.data.attributes)
-
-    //let newWeapon = new Weapon(weaponData, weaponData.attributes)
-    // calls the render in weapon class
-    document.querySelector('#weapon-container').innerHTML += newWeapon.renderweaponCard();
-  })
-}
-
-function patchWeapon(weapon, name, equipment_id) {
-  const bodyJSON = { name, equipment_id }
-  fetch(`http://localhost:3000/weapons/${weapon.id}`, {
-    method: 'PATCH',
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json',
-    },
-    body: JSON.stringify(bodyJSON),
-  })
-    .then(res => res.json())
-    //backend responds with new weapon instance as JSON
-    .then(updatedNote => console.log(updatedNote));
-}
-
-
-function render(weapon) {
-  const weaponMarkup = `
-          <div data-id=${weapon.id}>
-            <h3>${weapon.attributes.name}</h3>
-            <p>${weapon.attributes.equipment.name}</p>
-            <button data-id=${weapon.id}>edit</button>
-          </div>
-          <br><br>`;
-
-  document.querySelector('#weapon-container').innerHTML += weaponMarkup;
-}
---
+++ src/indexer.js
@@ -0,0 +1,7 @@
+//function to show all weapons
+
+//function to show all weapons by equipment
+//spooky, scratchy, splendid (1,2,3)
+
+//function to add a new weapon
+
