0e5237937eb22b8d1a8e5ffd4bb978b39c64514e
wonderfulkasey <kaseyarnold15@gmail.com>
2020-12-20 15:06:30 -0500

    trying to fix index/replace index

--- src/index.js
+++ src/index.js
@@ -2,41 +2,52 @@ const BASE_URL = "http://localhost:3000"
 const WEAPONS_URL = `${BASE_URL}/weapons`
 
 
+const allWeapons = document.querySelector("#weapon-collection")
+
 
 class Weapon {
 
   constructor(weaponAttributes) {
       this.id = id;
       this.name = weaponAttributes.name;
-      this.equipment = weaponAttributes.equipment;
+      this.equipment = weaponAttributes.equipment_id;
     }
   
     render() {
-      return `
-              <div class="card">
+      return `<div class="card">
                 <h3>${this.name}</h3>
-                <p>${this.equipment.name}</p>
-                <button data-id=${this.id}>edit</button>
+                <p>${this.equipment_id}</p>
+                <p>${this.id}</p>
               </div>`
 
 }}
 
 
-function fetchWeapons (){
+
+function showAllWeapons(weaponArray){
+  allWeapons.innerHTML = `<h2 class="subheader"> All Weapons </h2>
+                          <h3 class="all-link">View All</h2>`
+  weaponArray.forEach(weapon => {
+    allWeapons.innerHTML += new Weapon(weapon).render()
+  })
+}
+
+function fetchWeapons(){
   fetch(WEAPONS_URL)
   .then(res => res.json())
-  .then(weapons => renderWeapons(weapons))
+  .then(weapons => showAllWeapons(weapons))
 }
- 
 
-function renderWeapons(weapons) {
-     weapons.forEach(weapon => {
-            innerHTML += `<div class="card">
-              <h3>${weapon.attributes.name}</h3>
-              <p>${weapon.attributes.equipment.name}</p>
-              <button data-id=${weapon.id}>edit</button>
-              </br>
-            </div>`
-    })       
-} 
+
+//function to show all weapons
+//function to render all weapons
+//event listener to click and show
+
+
+//function to show all weapons by equipment
+//spooky, scratchy, splendid (1,2,3)
+//event listener to clickdown menu of 3 options
+
+//function to add a new weapon
+//event listener to put through
 
--- src/indexer.js
+++
@@ -1,7 +0,0 @@
-//function to show all weapons
-
-//function to show all weapons by equipment
-//spooky, scratchy, splendid (1,2,3)
-
-//function to add a new weapon
-
